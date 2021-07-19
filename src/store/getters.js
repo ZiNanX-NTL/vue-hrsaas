81fc9aaced81a531b2b1f5244b2c778d0d08f5d6
ZiNanX-NTL <702668400@qq.com>
2021-07-19 12:29:55 +0800

    '项目初始化'

---
+++ src/store/getters.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-19 10:01:33
+ * @LastEditTime: 2021-07-19 12:05:29
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \hrsaas\src\store\getters.js
+ */
+const getters = {
+  sidebar: state => state.app.sidebar,
+  device: state => state.app.device
+}
+export default getters
---
+++ src/store/modules/user.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-19 10:01:33
+ * @LastEditTime: 2021-07-19 12:03:34
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \hrsaas\src\store\modules\user.js
+ */
+
+export default {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {}
+}
+
